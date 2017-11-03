db37e6f260c7d1831a8438db3ed19547942e7d69
Hendricks <dubmore@gmail.com>
2017-11-02 14:22:26 -1000

    commented counter.py

--- counter.py
+++ counter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+###################################
+# tldr: move led in a square pattern
+###################################
+
 import math
 from struct import *
 import socket, sys
@@ -8,33 +12,40 @@ UDP_IP = "192.168.1.1"
 UDP_PORT = 9002
 
 
+# led to light up
 dumb = 1
+
+# socket to send on
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
+# move led functions
 def goUp(dumb):
     dumb  = dumb << 16;
-    sock.sendto(pack("Q", dumb),(UDP_IP, UDP_PORT))
-    time.sleep(0.1);
+    sendIt(dumb)
     return dumb
 
 def goDown(dumb):
     dumb = dumb >> 16;
-    sock.sendto(pack("Q", dumb),(UDP_IP, UDP_PORT))
-    time.sleep(0.1);
+    sendIt(dumb)
     return dumb
 
 def goLeft(dumb):
     dumb = dumb << 1;
-    sock.sendto(pack("Q", dumb),(UDP_IP, UDP_PORT))
-    time.sleep(0.1);
-    return dumb
-def goRight(dumb):
-    dumb = dumb >> 1;
-    sock.sendto(pack("Q", dumb),(UDP_IP, UDP_PORT))
-    time.sleep(0.1); 
+    sendIt(dumb)
     return dumb
 
-while True:
+def goRight(dumb):
+    dumb = dumb >> 1;
+    sendIt(dumb)
+    return dumb
+
+# send led enoding on socket
+def sendIt(dumb):
+    sock.sendto(pack("Q", dumb),(UDP_IP, UDP_PORT))
+    time.sleep(0.1);
+
+# move led around
+def dance(dumb):
     dumb = goLeft(dumb)
     dumb = goLeft(dumb)
     dumb = goLeft(dumb)
@@ -47,3 +58,10 @@ while True:
     dumb = goDown(dumb)
     dumb = goDown(dumb)
     dumb = goDown(dumb)
+
+def main():
+    dance(dumb)
+
+if __name__ == "__main__":
+    while True:
+       main()
